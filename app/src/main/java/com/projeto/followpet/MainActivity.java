7d2f62bff8e57a91a82467e6f896cdd8308a32ad
Eduardo Lima <eduardo.psg30@gmail.com>
2016-11-04 22:26:04 -0300

    codigo comentado

--- app/src/main/java/com/projeto/followpet/MainActivity.java
+++ app/src/main/java/com/projeto/followpet/MainActivity.java
@@ -9,33 +9,38 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar mToolbar;
+    private Toolbar mToolbar;//Variavel necessaria para usar o toolbar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
-        mToolbar.setTitle(R.string.app_name);
+        // TODO Toolbar main
+        /* o toolbar é o sucessor do actionbar, ele é o a barra que aparece
+            no topo da tela
+        */
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_main); //recupera usa variavel para recupera o layout na activity_main
+        mToolbar.setTitle(R.string.app_name);//define em codigo java o titulo do toolbar
         //mToolbar.setLogo(R.drawable.ic_inter);
 
-        setSupportActionBar(mToolbar);
-
+        setSupportActionBar(mToolbar);//iniciar o toolbar
+        //funçoes adicionais, deve consulta documentação
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.ic_inter);
+        getSupportActionBar().setLogo(R.drawable.ic_inter);// defina o icon de logo para toolbar
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-
+        //fim do toolbar
     }
-
+    //TODO Menu de incones laterais do main
+    /*os codigos ai em baixo sao ultilizado para setar os icon de funçao
+    * no lado direito do toolbar*/
     @Override
     public  boolean onCreateOptionsMenu(Menu menu){
 
-        getMenuInflater().inflate(R.menu.menu_tb_main, menu);
+        getMenuInflater().inflate(R.menu.menu_tb_main, menu);// inicia os icon no toolbar
 
         return true;
     }
-
+    //funçao para saber qual incone foi clickado e o deve fazer
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent it = null;
@@ -60,4 +65,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+    //fim Menu de incones laterais do main
 }

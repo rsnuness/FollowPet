c16bce3d11bc11d16376d2ef6f29f0a11609bb14
Eduardo Lima <eduardo.psg30@gmail.com>
2016-11-22 05:44:54 -0300

    db 02

--- app/src/main/java/com/projeto/followpet/DbManager.java
+++ app/src/main/java/com/projeto/followpet/DbManager.java
@@ -1,19 +1,23 @@
 package com.projeto.followpet;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 /**
  * Created by eduardo on 20/11/16.
  */
 
 public class DbManager {
-    private static  DbFollowpet dbfollowpet = null;
+
+    private SQLiteDatabase db;
+    private DbFollowpet banco;
+
+
 
     public  DbManager(Context context){
-        if (dbfollowpet == null){
-            dbfollowpet = new DbFollowpet(context);
-        }
+        banco = new DbFollowpet(context);
     }
 
+    public String inserePet(String nome, String data_nascimento)
 
 }
